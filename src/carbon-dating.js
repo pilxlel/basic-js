6ff32920f82a1db67d4261c33f02922cb4067b53
Andrew Homan <andrew@Andrews-MacBook-Pro.local>
2023-12-21 15:59:29 +0400

    feat: implement carbon dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const K = 0.693 / HALF_LIFE_PERIOD;
+  const DATE = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / K);
+  
+  if (typeof sampleActivity !== 'string') return false;
+  if (isNaN(sampleActivity)) return false;
+  if (!isFinite(DATE)) return false;
+  if (DATE < 0) return false;
+  return DATE;
 }
 
 module.exports = {
