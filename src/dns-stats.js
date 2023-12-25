eac1a2b8bd46df45dcfdbab09ef968979178a152
Andrew Homan <andrew@Andrews-MacBook-Pro.local>
2023-12-25 14:55:06 +0400

    feat: impement getDNSStats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    const domainParts = domains[i].split('.').reverse();
+    let currentDomain = '';
+
+    for (let j = 0; j < domainParts.length; j++) {
+      currentDomain += `.${domainParts[j]}`;
+      if (dnsStats.hasOwnProperty(currentDomain)) {
+        dnsStats[currentDomain] += 1;
+      } else {
+        dnsStats[currentDomain] = 1;
+      }
+    }
+  }
+
+  return dnsStats;
 }
 
 module.exports = {
