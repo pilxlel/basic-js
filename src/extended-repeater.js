62804e68827e5832ca760c0580fd295bd37a8925
Andrew Homan <andrew@Andrews-MacBook-Pro.local>
2023-12-25 15:13:06 +0400

    feat: partly implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let modStr = '' + str;
+  let result = '';
+  let additionResult = '';
+
+  //задает количество повторений строки;
+  let repeatTimes;
+  'repeatTimes' in options ? repeatTimes = options.repeatTimes : repeatTimes = 0;
+
+  //строка, разделяющая повторы строки;
+  let separator;
+  'separator' in options ? separator = options.separator : separator = '+';
+
+  const separatorLength = separator.length;
+
+  //дополнительная строка, которая будет добавлена к каждому повторению строки;
+  let addition;
+  'addition' in options ? addition = '' + options.addition : addition = '';
+
+  //задает количество повторений дополнения;
+  let additionRepeatTimes;
+  'additionRepeatTimes' in options ? additionRepeatTimes = options.additionRepeatTimes : additionRepeatTimes = 0;
+
+  //строка, разделяющая повторы дополнения.
+  let additionSeparator;
+  'additionSeparator' in options ? additionSeparator = options.additionSeparator : additionSeparator = '|'
+
+  const additionSeparatorLength = additionSeparator.length;
+  let isFirstCount = true;
+
+  if (repeatTimes > 0 && additionRepeatTimes > 0) {
+    for (let i = 0; i < repeatTimes; i++) {
+      if (additionRepeatTimes > 0 && isFirstCount) {
+        for (let j = 0; j < additionRepeatTimes; j++) {
+          additionResult += addition + additionSeparator;
+        }
+        isFirstCount = false;
+        additionResult = additionResult.slice(0, -additionSeparatorLength);
+      }
+      result += modStr + additionResult + separator;
+    }
+    return result.slice(0, -separatorLength);
+  }
+
+  if (repeatTimes === 0 && additionRepeatTimes === 0) {
+    result += modStr + addition;
+    return result
+  }
+
+  if (repeatTimes > 0 && additionRepeatTimes === 0) {
+    for (let i = 0; i < repeatTimes; i++) {
+      result += modStr + additionResult + separator;
+    }
+    return result.slice(0, -separatorLength);
+  }
 }
 
 module.exports = {
